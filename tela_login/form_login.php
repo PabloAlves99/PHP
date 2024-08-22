e9bbfef8879c6131718b243518b814a957583365
Pablo <PabloHAlves99@gmail.com>
2024-08-22 00:56:06 -0300

    Tela de login

---
+++ tela_login/form_login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Login feito</title>
+</head>
+<body>
+    <header>
+        <h1>
+            Login feito
+        </h1>
+    </header>
+    <main>
+        <?php
+        $name = $_POST["nome"];
+        $last_name = $_POST["sobrenome"];
+
+        $username = strtolower($name."_".$last_name);
+
+        echo "Olá $name $last_name. Seu usuário é: $username"
+        ?>
+    </main>
+</body>
+</html>
