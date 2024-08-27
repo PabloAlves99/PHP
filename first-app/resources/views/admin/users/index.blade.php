03e5f5948468a14088731aad3da6fc362c06aee9
Pablo <PabloHAlves99@gmail.com>
2024-08-26 21:27:50 -0300

    view registered users

---
+++ first-app/resources/views/admin/users/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registered users</title>
+</head>
+<body>
+    <h1> Usuarios </h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($users as $user)
+                <tr>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>-</td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="100">Nenhum usuario no banco</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+    {{ $users->links() }}
+</body>
+</html>
